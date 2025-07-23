b04f702f99be6081229b1a2313ad0406a8a145cf
Nigel de Vos <zuperpap@hotmail.com>
2025-07-23 14:50:06 +0200

    changed correct usage of dynamic user

--- Install-WSL-W11.bat
+++ Install-WSL-W11.bat
@@ -1,10 +1,9 @@
 winget update --all
 wsl --install --d Ubuntu
-
-@echo off
-REM Get the current user's home directory
+REM #Get current user profile
 set userProfile=%USERPROFILE%
 
-REM Append WSL configuration settings to the .wslconfig file
+REM #Append WSL configuration settings to the .wslconfig file
 echo [wsl2] >> "%userProfile%\.wslconfig"
+echo. >> "%userProfile%\.wslconfig"
 echo networkingMode=Mirrored >> "%userProfile%\.wslconfig"
